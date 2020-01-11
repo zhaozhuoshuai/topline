e9b1a9350a3014023cac5ecfa58a4ef66c81beb9
zhuoshuai <zhuoshuai88@163.com>
2020-01-11 15:32:06 +0800

    Revert "分页-筛选-过滤器-401码解决"
    This reverts commit b5d9b4bee0286ec6df08962edb170e7afa931272.

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -13,17 +13,12 @@
           <el-radio v-model="searchForm.status" label="3">审核失败</el-radio>
         </el-form-item>
         <el-form-item label="频道列表:">
-          <el-select
-          v-model="searchForm.channel_id"
-          placeholder="请选择"
-          clearable
-          @change="getArticleList()">
+          <el-select v-model="searchForm.channel_id" placeholder="请选择" clearable>
             <el-option
-              v-for="item in channelList"
-              :key="item.id"
-              :label="item.name"
-              :value="item.id"
-            ></el-option>
+            v-for="item in channelList"
+            :key="item.id"
+            :label="item.name"
+            :value="item.id"></el-option>
           </el-select>
         </el-form-item>
         <el-form-item label="时间选择:">
@@ -38,46 +33,6 @@
         </el-form-item>
       </el-form>
     </el-card>
-    <el-card class="box-card">
-      <div slot="header" class="clearfix">
-        <span>共找到{{tot}}条符合条件的内容</span>
-        <el-table :data="articleList" style="width:100%;">
-          <el-table-column label="图标">
-            <img
-              :src="stData.row.cover.images[0]"
-              alt="没有图标"
-              slot-scope="stData
-            "
-              width="150"
-              height="100"
-            />
-          </el-table-column>
-          <el-table-column prop="title" label="标题"></el-table-column>
-          <el-table-column prop="status" label="状态">
-            <template slot-scope="stData">
-              <el-tag v-if="stData.row.status===0">草稿</el-tag>
-              <el-tag v-else-if="stData.row.status===1" type="success">待审核</el-tag>
-              <el-tag v-else-if="stData.row.status===2" type="info">审核通过</el-tag>
-              <el-tag v-else-if="stData.row.status===3" type="warning">审核失败</el-tag>
-            </template>
-          </el-table-column>
-          <el-table-column prop="pubdate" label="发布时间"></el-table-column>
-          <el-table-column label="操作">
-            <el-button type="primary" size="mini" icon="el-icon-edit">修改</el-button>
-            <el-button type="danger" size="mini" icon="el-icon-delete">删除</el-button>
-          </el-table-column>
-        </el-table>
-        <el-pagination
-          @size-change="handleSizeChange"
-          @current-change="handleCurrentChange"
-          :current-page="searchForm.page"
-          :page-sizes="[10, 20, 30, 40]"
-          :page-size="searchForm.per_page"
-          layout="total, sizes, prev, pager, next, jumper"
-          :total="tot"
-        ></el-pagination>
-      </div>
-    </el-card>
   </div>
 </template>
 
@@ -85,25 +40,17 @@
 export default {
   name: 'Article',
   watch: {
-    searchForm: {
-      handler: function (newV, oldV) {
-        this.getArticleList()
-      },
-      deep: true
-    },
     // 被检测的成员:function(newV,oldV)
     // newV:数据变化后的样子
     // oldV:数据变化前的样子
-    timetotime (newval) {
-      if (newval) {
-        this.searchForm.begin_pubdate = newval[0]
-        this.searchForm.end_pubdate = newval[1]
+    timetotime: function (newV, oldV) {
+      if (newV) {
+        this.searchForm.begin_pubdate = newV[0]
+        this.searchForm.end_pubdate = newV[1]
       } else {
         this.searchForm.begin_pubdate = ''
         this.searchForm.end_pubdate = ''
       }
-      // 根据最新的时间范围,获得对应文章信息
-      this.getArticleList()
     }
   },
   data () {
@@ -114,12 +61,11 @@ export default {
       timetotime: '', // 时间范围临时接收成员
       // 检索表单数据对象
       searchForm: {
-        page: 1, // 当前数据记录页码
-        per_page: 10, // 分页数据记录条数(10~50)
         begin_pubdate: '', // 开始时间
         end_pubdate: '', // 结束时间
         channel_id: '', // 频道id
         status: '' // 文章状态,
+
       }
     }
   },
@@ -130,16 +76,6 @@ export default {
     this.getArticleList()
   },
   methods: {
-    // 分页相关
-    // 每条条数变化的回调处理
-    handleSizeChange (val) {
-      console.log(val)
-      this.searchForm.per_page = val
-    },
-    // 页码变化的回调处理
-    handleCurrentChange (val) {
-      this.searchForm.page = val
-    },
     // 获取真实频道列表数据
     getChannelList () {
       let pro = this.$http({
@@ -156,18 +92,9 @@ export default {
     },
     // 获取真实文章信息数据
     getArticleList () {
-      let searchData = {}
-      for (var i in this.searchForm) {
-        // i:代表对象的成员属性名称, status、channel_id、begin_pudate等等
-        if (this.searchForm[i] || this.searchForm[i] === 0) {
-          // 成员值非空
-          searchData[i] = this.searchForm[i]
-        }
-      }
       let pro = this.$http({
         url: '/mp/v1_0/articles',
-        method: 'get',
-        params: searchData
+        method: 'get'
       })
       pro
         .then(result => {
@@ -182,15 +109,9 @@ export default {
         })
     }
   }
+
 }
 </script>
 
 <style lang="less" scoped>
-.box-card {
-  margin-bottom: 15px;
-}
-.el-pagination{
-  margin-top: 15px;
-  text-align: center;
-}
 </style>
